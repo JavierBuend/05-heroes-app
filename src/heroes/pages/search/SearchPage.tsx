d18ae55f18872c6277708ccbf68591a478d02a73
Javier <186595036+cepex1@users.noreply.github.com>
2026-05-18 13:22:07 +0200

    componentes reutilizables

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,6 +1,14 @@
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <>
+      <CustomJumbotron
+        title="Búsqueda de superheroes"
+        description="Descubre, explora y administra superheroes y villanos"
+      />
+    </>
+  );
 };
 
 export default SearchPage;
